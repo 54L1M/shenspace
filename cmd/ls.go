d89e5bee7c7c19258dc693294b6afaf9855de8ec
Shen <59168670+0xshen@users.noreply.github.com>
2023-04-17 12:47:16 +0000

    start project

---
+++ cmd/ls.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// lsCmd represents the ls command
+var lsCmd = &cobra.Command{
+	Use:   "ls",
+	Short: "Lists all projects.",
+	Long:  `Lists all directories in the $PROJECTS_HOME directory.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		name, _ := os.UserHomeDir()
+		dirs, err := os.ReadDir(name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, dir := range dirs {
+			if dir.IsDir() {
+				fmt.Println(dir.Name())
+			}
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(lsCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// lsCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// lsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/0xshen/shenspace/cmd"
+
+func main() {
+	cmd.Execute()
+}
